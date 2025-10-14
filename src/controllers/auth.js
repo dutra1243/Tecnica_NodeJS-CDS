781c7c6d9d2a3fed4a5d3219e5ef2a0ffb0024ef
dutra1243 <facundodutraextra@gmail.com>
2025-10-14 16:04:14 -0300

    auth implemented

---
+++ src/controllers/auth.js
@@ -0,0 +1,91 @@
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const jwt = require('jsonwebtoken');
+
+const { readFile, writeFile } = require('../helpFunctions/fileHelper');
+
+const USERS_FILE = 'DATA/USERS.txt';
+const BLACKLIST_FILE = 'DATA/BLACKLIST.txt';
+const SECRET_KEY = process.env.JWT_SECRET_KEY
+
+async function register(req, res, next) {
+    try {
+        const { email, firstName, lastName, password } = req.body;
+        const users = readFile(USERS_FILE);
+
+        if (users.find(user => user.email === email)) {
+            return res.status(400).json({ error: 'Email already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = {
+            id: uuid.v4(),
+            email,
+            firstName,
+            lastName,
+            password: hashedPassword,
+        };
+
+        users.push(newUser);
+        writeFile(USERS_FILE, users);
+        res.status(201).json({ message: 'User registered successfully' });
+    }
+    catch (error) {
+        console.error('Registration error:', error);
+        next(error);
+    }
+}
+
+async function login(req, res, next) {
+    try {
+        const { email, password } = req.body;
+        const users = readFile(USERS_FILE);
+        const user = users.find(user => user.email === email);
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const token = jwt.sign({ id: user.id, email: user.email, jti: uuid.v4() }, SECRET_KEY, {
+            expiresIn: '48h'
+        })
+
+        res.status(200).json({
+            message: 'Login successful',
+            token
+        });
+    }
+    catch (error) {
+        console.error('Login error:', error);
+        next(error);
+    }
+}
+
+async function logout(req, res, next) {
+    try {
+        const token = req.headers['authorization']?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+
+        const jti = jwt.decode(token).jti;
+        const blacklist = readFile(BLACKLIST_FILE);
+
+        if (!blacklist.includes(jti)) {
+            blacklist.push(jti);
+            writeFile(BLACKLIST_FILE, blacklist);
+        }
+
+        res.status(200).json({ message: 'Logout successful' });
+    }
+    catch (error) {
+        console.error('Logout error:', error);
+        next(error);
+    }
+}
+
+module.exports = { register, login, logout };
---
+++ src/routes/auth.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { register, login, logout } = require('../controllers/auth');
+const { body } = require('express-validator');
+const { validateReq } = require('../middleware/validateReq');
+
+const router = express.Router();
+
+/**
+ * @route POST /api/auth/register
+ * @summary Registers a new user
+ * @param {string} email.body.required - User's email address (must be a valid email)
+ * @param {string} firstName.body.required - User's first name
+ * @param {string} lastName.body.required - User's last name
+ * @param {string} password.body.required - User's password (minimum 8 characters)
+ * @returns {object} 201 - User registered successfully
+ * @returns {object} 400 - Email already exists or validation error
+ * @returns {object} 500 - Internal server error
+ */
+router.post('/auth/register', [
+    body('email').isEmail().withMessage('Invalid email format'),
+    body('firstName').notEmpty().withMessage('First name is required').escape(),
+    body('lastName').notEmpty().withMessage('Last name is required').escape(),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+], validateReq, register);
+
+/**
+ * @route POST /api/auth/login
+ * @summary Authenticates a user and returns a JWT token
+ * @param {string} email.body.required - User's email address (must be a valid email)
+ * @param {string} password.body.required - User's password
+ * @returns {object} 200 - Login successful, returns JWT token
+ * @returns {object} 401 - Invalid email or password
+ * @returns {object} 500 - Internal server error
+ */
+router.post('/auth/login', [
+    body('email').isEmail().withMessage('Invalid email format'),
+    body('password').notEmpty().isLength({min : 8}).withMessage('Password must be at least 8 characters long')
+], validateReq, login);
+
+/**
+ * @route POST /api/auth/logout
+ * @summary Logs out a user
+ * @access  Protected (Bearer Token required)
+ * @headers Authorization: Bearer <token>
+ * @returns {object} 200 - Logout successful
+ * @returns {object} 500 - Internal server error
+ */
+router.post('/auth/logout', logout);
+
+module.exports = router;
