f576a8dc8ebf42d3992b964dade9ed9d18e657cf
dutra1243 <facundodutraextra@gmail.com>
2025-10-15 14:45:18 -0300

    movie controller done

---
+++ src/controllers/movieController.js
@@ -0,0 +1,46 @@
+const TMDB_TOKEN = process.env.TMDB_TOKEN;
+const axios = require('axios');
+
+async function searchMovies(req, res, next) {
+    try {
+        const query = req.query.keyword || '';
+        const keyword = query.trim();
+        console.log('Keyword:', keyword);
+
+        
+        let response = {};
+        try {
+            const baseURL = 'https://api.themoviedb.org/3';
+            const headers = {
+                'Authorization': `Bearer ${TMDB_TOKEN}`,
+                'Content-Type': 'application/json'
+            };
+
+            response = await axios.get(
+                keyword 
+                    ? `${baseURL}/search/movie?query=${encodeURIComponent(keyword)}&page=1`
+                    : `${baseURL}/movie/popular`,
+                { headers }
+            );
+        } catch (error) {
+            console.error('Error fetching movies:', error.message);
+            throw error;
+        }
+
+
+        const addedScore = response.data.results
+        .map(movie => ({
+            ...movie,
+            suggestionScore: Math.floor(Math.random() * 100)
+        }))
+        .sort((a, b) => b.suggestionScore - a.suggestionScore);
+
+        res.status(200).json(addedScore);
+
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        next(error);
+    }
+}
+
+module.exports = { searchMovies };
